36204b1f02ec27f0ea1de9a0172b1522af40a78f
Justin Grote <justin@grote.name>
2015-11-05 12:42:22 -0800

    Added Get-SSLCertificate, GooglePhotos-Test and Create-AzureRMNSG

---
+++ Get-SSLCertificate.ps1
@@ -0,0 +1,46 @@
+﻿[CmdletBinding()]
+
+param([parameter(Mandatory=$true,ValueFromPipeline=$true)][string[]]$HostName,[int]$port=443)
+
+
+process { foreach ($computername in $HostName) {
+    #Create a TCP Socket to the computer and a port number
+    write-verbose "Connecting to $computername on $port"
+    $tcpsocket = New-Object Net.Sockets.TcpClient($computerName, $port)
+
+    #test if the socket got connected
+    if(!$tcpsocket)
+    {
+        Write-Error "Error Opening Connection: $port on $computername Unreachable"
+        exit 1
+    }
+    else
+    {
+        #Socket Got connected get the tcp stream ready to read the certificate
+        write-verbose "Successfully Connected to $computername on $port" 
+        $tcpstream = $tcpsocket.GetStream()
+        Write-verbose "Reading SSL Certificate...." 
+        #Create an SSL Connection 
+        $sslStream = New-Object System.Net.Security.SslStream($tcpstream,$false)
+        #Force the SSL Connection to send us the certificate
+        $sslStream.AuthenticateAsClient($computerName)
+
+        #Read the certificate
+        $certinfo = New-Object system.security.cryptography.x509certificates.x509certificate2($sslStream.RemoteCertificate)
+    }
+
+    $returnobj = [ordered]@{
+        ComputerName = $computername
+        Port = $port
+        Subject = $certinfo.Subject;
+        Issuer = $certinfo.Issuer;
+        DNSNameList = $certinfo.DnsNameList;
+        EnhancedKeyUsageList = $certinfo.EnhancedKeyUsageList;
+        SerialNumber = $certinfo.SerialNumber;
+        Thumbprint = $certinfo.Thumbprint;
+        Issued = $certinfo.NotBefore
+        Expires = $certinfo.NotAfter
+    }
+
+    new-object PSCustomObject -Property $returnobj
+} }
--- GooglePhotos-Test.ps1
+++ GooglePhotos-Test.ps1
@@ -1,21 +1,37 @@
-﻿function Connect-GoogleAPI {
-    <#
-    .SYNOPSIS
-    Connects to the Google API using OAuth 2.0 and stores the access tokens in global variables.
-    #>
+﻿### HELPER FUNCTIONS
 
+function Connect-GPhotosAPI {
+<#
+    .SYNOPSIS
+    Connects to the Google API using OAuth 2.0 and stores the access information in the $GPhotosAuthInfo variable.
+#>
+
+    [CmdletBinding()]
+    #Please don't use this for other apps, or I'll have to revoke it and force users to put in their own. Thanks :)
     $clientId = "1053883683818-0g862dbhqqmpqeatuh1ig8jectagha9n.apps.googleusercontent.com"   
     $clientSecret = "TOorIeeepy2TrEDCQtWXCdJY"
-    $scope = "https://picasaweb.google.com/data/"
+    #$scope = "https://picasaweb.google.com/data/"
+    $scope="https://www.googleapis.com/auth/userinfo%23email+https://mail.google.com/+https://www.googleapis.com/auth/photos+https://www.google.com/m8/feeds/+https://picasaweb.google.com/c/+https://www.googleapis.com/auth/plus.stream.write+https://www.googleapis.com/auth/plus.circles.read+https://www.googleapis.com/auth/plus.profiles.read+https://www.googleapis.com/auth/plus.me+https://www.googleapis.com/auth/plus.media.upload+https://www.googleapis.com/auth/plus.media.readonly+https://www.googleapis.com/auth/plus.settings+http://gdata.youtube.com"
     $redirectUri = "urn:ietf:wg:oauth:2.0:oob:auto"
-
-    # sign in form will appear only once
     $authorizeUrl = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=$clientId&scope=$scope&redirect_uri=$redirectUri"
-
-    # sign in form will appear every time
-    # $authorizeUrl = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&state=$csrfToken&scope=$scope&approval_prompt=force"
-
     $accessTokenUrl = "https://accounts.google.com/o/oauth2/token"
+
+    #If a token already exists and it is not expired, don't do anything
+    if ($GPhotosAuthInfo) {
+        if ($GPhotosAuthInfo.expirydate -lt (Get-Date)) { 
+            write-debug "GAPI Auth Token $($GPhotosAuthInfo.AccessToken) is expired, refreshing..."
+            $RefreshTokenResponse = Invoke-RestMethod -ErrorAction stop -Method Post -Uri $accessTokenUrl -ContentType "application/x-www-form-urlencoded" `
+                -Body @{client_id=$clientId; client_secret=$clientSecret; redirect_uri=$redirectUri; grant_type="refresh_token"; refresh_token=$GPhotosAuthInfo.refreshToken} 
+            $GPhotosAuthInfo.accessToken = $RefreshTokenResponse.access_token
+            $GPhotosAuthInfo.expiryDate = (get-date).AddSeconds($RefreshTokenResponse.expires_in)
+            write-debug "GAPI Token Refreshed and Token Updated"
+            return
+        }
+        else {
+            write-debug "GAPI Auth Token Exists and is current"
+            return
+        }
+    }
 
 
     # step 1: make a GET request for authorization code (will need to log in Google account if not already logged in)
@@ -50,20 +66,99 @@
 
         $accessToken = $response.access_token
         $refreshToken = $response.refresh_token
+        $expiryDate = (get-date).AddSeconds($response.expires_in)
 
         # step 3: make future GET requests within this scope with the access token
 
-        $requestHeader = @{"Authorization"="Bearer $accessToken"}
+        $requestHeader = @{
+            "Authorization"="Bearer $accessToken"
+            "User-Agent"="Google Photos Powershell Module"
+        }
 
 
         # step x: make a POST request to refresh access token
 
-        #$response = Invoke-RestMethod -Method Post -Uri $accessTokenUrl -ContentType "application/x-www-form-urlencoded" `
-        #    -Body @{client_id=$clientId; client_secret=$clientSecret; redirect_uri=$redirectUri; grant_type="refresh_token"; refresh_token=$refreshToken}
+  }
+
+    $tokenProps =  [ordered]@{}
+    $tokenProps.requestHeader = $requestHeader
+    $tokenProps.refreshToken = $refreshToken
+    $tokenProps.accessToken = $accessToken
+    $tokenProps.expiryDate = $expiryDate
+
+    New-Variable -Scope Script -Name GPhotosAuthInfo -Value (new-object PSCustomObject -property $tokenprops) -Force
+}
+
+function Merge-HashTable {
+    param(
+        [hashtable] $default, 
+        [hashtable] $uppend
+    )
+
+    # clone for idempotence
+    $default1 = $default.Clone() ;
+
+    # remove any keys that exists in original set
+    foreach ($key in $uppend.Keys) {
+        if ($default1.ContainsKey($key)) {
+            $default1.Remove($key) ;
+        }
+    }
+
+    # union both sets
+    return $default1 + $uppend ;
+}
+
+function Invoke-GPhotosRequest {
+    <#
+    .SYNOPSIS
+    Helper function wrapper around invoke-restmethod to streamline requests and include error checking for token lifetime
+    #>
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        
+        [Parameter(Mandatory=$true)]$GAPIPath,
+        [HashTable]$Headers,
+        $Body,
+        $Method = "GET",
+        $GAPIHost = "www.googleapis.com",
+        $ContentType = "application/json; charset=utf-8",
+        $UserAgent = $GPhotosAuthInfo.requestHeader.'User-Agent'
+    )
+
+    begin { Connect-GPhotosAPI }
+
+    process {
+        if ($Headers) {$Headers = Merge-HashTable $GPhotosAuthInfo.requestHeader $Headers}
+        else {$Headers = $GPhotosAuthInfo.requestHeader}
+
+        $RequestURI = "https://" + $GAPIHost + $GAPIPath
+        $RESTRequestParams = @{
+            Method = $Method;
+            URI = $RequestURI;
+            ContentType = $ContentType;
+            UserAgent = $UserAgent;
+            Headers = $Headers;
+        }
+
+        return invoke-restmethod @RESTRequestParams -Body $Body
     }
 }
 
+### USERSPACE FUNCTIONS
 
-function Get-GPhotosAlbum {
+
+
+
+
+
+
+function Test-GPhotosPhotoUploaded {
+    <#
+    .SYNOPSIS
+    Connects to the Google API using OAuth 2.0 and stores the access tokens in global variables.
+    #>
     $uri = "https://picasaweb.google.com/data/feed/api/user/justingrote"
+
 }
