40a325701501dea6a9fe5af5fe8886de42e19586
Justin Grote <justin@grote.name>
2015-11-05 14:23:20 -0800

    Updates to SSLCertificate

--- Get-SSLCertificate.ps1
+++ Get-SSLCertificate.ps1
@@ -1,45 +1,30 @@
 ﻿[CmdletBinding()]
+param(
+        [parameter(Mandatory,ValueFromPipeline)][string[]]$ComputerName,
+        [int]$TCPPort=443,
+        [int]$Timeoutms=3000
+)
 
-param([parameter(Mandatory=$true,ValueFromPipeline=$true)][string[]]$HostName,[int]$port=443)
 
-
-process { foreach ($computername in $HostName) {
-    #Create a TCP Socket to the computer and a port number
-    write-verbose "Connecting to $computername on $port"
-    $tcpsocket = New-Object Net.Sockets.TcpClient($computerName, $port)
-
-    #test if the socket got connected
-    if(!$tcpsocket)
-    {
-        Write-Error "Error Opening Connection: $port on $computername Unreachable"
-        exit 1
-    }
-    else
-    {
-        #Socket Got connected get the tcp stream ready to read the certificate
-        write-verbose "Successfully Connected to $computername on $port" 
-        $tcpstream = $tcpsocket.GetStream()
-        Write-verbose "Reading SSL Certificate...." 
-        #Create an SSL Connection 
-        $sslStream = New-Object System.Net.Security.SslStream($tcpstream,$false)
-        #Force the SSL Connection to send us the certificate
-        $sslStream.AuthenticateAsClient($computerName)
-
-        #Read the certificate
-        $certinfo = New-Object system.security.cryptography.x509certificates.x509certificate2($sslStream.RemoteCertificate)
-    }
+process { foreach ($computer in $computerName) {
+    $port = $TCPPort
+    write-verbose "$computer`: Connecting on port $port"
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    $req = [Net.HttpWebRequest]::Create("https://$computer`:$port/")
+    $req.Timeout = $Timeoutms
+    try {$req.GetResponse() | Out-Null} catch {write-error "Couldn't connect to $computer on port $port"; continue}
+    if (!($req.ServicePoint.Certificate)) {write-error "No Certificate returned on $computer"; continue}
+    $certinfo = $req.ServicePoint.Certificate
 
     $returnobj = [ordered]@{
-        ComputerName = $computername
-        Port = $port
+        ComputerName = $computer;
+        Port = $port;
         Subject = $certinfo.Subject;
+        Thumbprint = $certinfo.GetCertHashString();
         Issuer = $certinfo.Issuer;
-        DNSNameList = $certinfo.DnsNameList;
-        EnhancedKeyUsageList = $certinfo.EnhancedKeyUsageList;
-        SerialNumber = $certinfo.SerialNumber;
-        Thumbprint = $certinfo.Thumbprint;
-        Issued = $certinfo.NotBefore
-        Expires = $certinfo.NotAfter
+        SerialNumber = $certinfo.GetSerialNumberString();
+        Issued = [DateTime]$certinfo.GetEffectiveDateString();
+        Expires = [DateTime]$certinfo.GetExpirationDateString();
     }
 
     new-object PSCustomObject -Property $returnobj
