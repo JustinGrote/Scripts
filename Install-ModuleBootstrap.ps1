0b0460a6ec771788b54571e95b520537ff6d1e6c
Grote <jgrote@allieddigital.net>
2017-10-11 09:54:42 -0700

    Remove PScmdlet Whatif for Azure Function Compatability

--- Install-ModuleBootstrap.ps1
+++ Install-ModuleBootstrap.ps1
@@ -15,7 +15,7 @@ Install-ModuleBootstrap.ps1
 Installs PSDeploy to your default CurrentUser modules directory
 
 .EXAMPLE
-Invoke-Command -ArgumentList "PSDeploy" -ScriptBlock ([scriptblock]::Create((new-object net.webclient).DownloadString('http://tinyurl.com/PSIMB'))) -ArgumentList "Pester"
+Invoke-Command -ArgumentList "PSDeploy" -ScriptBlock ([scriptblock]::Create((new-object net.webclient).DownloadString('http://tinyurl.com/PSIMB')))
 Bootstraps this script from a URL. Useful for Azure Functions
 
 #>
@@ -91,7 +91,7 @@ if(-not ($NugetPath = (Get-Command 'nuget.exe' -ErrorAction SilentlyContinue).Pa
 foreach ($moduleNameItem in $Name) {
     if ( (-not (test-path (join-path $Path $ModuleNameItem))) -or ($Force) ) {
         write-verbose "Installing Module $moduleNameItem to $Path"
-        if ($PSCmdlet.ShouldProcess( (join-path $path $moduleNameItem), "Install Powershell Module $moduleNameItem via nuget.exe")) {
+        #if ($PSCmdlet.ShouldProcess( (join-path $path $moduleNameItem), "Install Powershell Module $moduleNameItem via nuget.exe")) {
             $NugetParams = 'install', "$moduleNameItem", '-Source', 'https://www.powershellgallery.com/api/v2/',
                             '-ExcludeVersion', '-NonInteractive', '-Verbosity', 'quiet', '-OutputDirectory', $Path
             & $NugetPath @NugetParams | write-verbose
@@ -100,7 +100,7 @@ foreach ($moduleNameItem in $Name) {
             if ($moduleNameItem -match 'PSDepend') {
                 copy-item $NugetPath $Path\$moduleNameItem -force | out-string | write-verbose
             }
-        }
+        #}
     } else {
         write-verbose "Module $moduleNameItem already exists at $Path and -Force not specified, skipping..."
     }
